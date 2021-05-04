6192175285df95aa56323235b9ce9f7e8c86fc3e
Kristian Sofus Knudsen <blobyfloby@gmail.com>
2021-05-04 21:44:04 +0200

    First proof-of-concept draft of the GUI

--- src/main/java/dtu/scheduler/Main.java
+++ src/main/java/dtu/scheduler/Main.java
@@ -14,13 +14,51 @@ public class Main {
 	);
 
 	public static void main(String args[]) throws Exception {
+		boolean programRunning = true;
+		while (programRunning) {
 
+			//Scenes are organized linearly a few cases (login -> mainMenu), but otherwise and mostly in a tree-like structure:
+			//If entering the "Register Hours" scene from the "Main Menu" scene, when returning, you return to the "Main Menu" scene
+			//If returning from the "Main Menu" scene, you loop back to the login scene, unless the program is quit
+
+			//Program starts with logging in
+			loginScene();
+			//Once logged in, you arrive at the main menu
+			//Other scenes are accessed from there
+			mainMenuScene();
+		}
+	}
+
+	public static void mainMenuScene() {
 		gui.clearScreen();
 
-		loginScene();
+		gui.println("Welcome " + schedulingApp.getCurrentUser());
+
+		String[] mainMenuOptions = { //Examples, more to be added. Remember to update the switch case below
+			"Register hours on activity",
+			"Change registered hours",
+			"Logout"
+		};
+		while (true) {
+		int menuChoice = gui.numericalMenu(mainMenuOptions);
+
+		switch (menuChoice) {
+			case 1:
+				gui.println("Not implemented");
+				break;
+			case 2:
+				gui.println("Not implemented");
+				break;
+			case 3:
+				schedulingApp.logOut();
+				return; // Return to main, initiates new login
+		}
+		}
 	}
 
 	public static void loginScene() throws WorkerDoesNotExistException {
+		gui.clearScreen();
+
 		gui.println("Enter user id:");
 		while (true) {
 		String user_id = gui.inputString();
@@ -32,9 +70,5 @@ public class Main {
 			gui.clearScreen();
 			gui.println("No user exists with given id, please try again:");
 		}}
-
-		gui.clearScreen();
-		gui.println("Successfully logged in!");
-		gui.inputString();
 	}
 }
