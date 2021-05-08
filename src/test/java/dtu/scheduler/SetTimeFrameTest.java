43694fa21214b68473d43dadf6ac120b3413e63d
Mads Harder <mads987a@gmail.com>
2021-05-09 01:49:48 +0200

    Now test that you can add time frame to Project

--- src/test/java/dtu/scheduler/SetTimeFrameTest.java
+++ src/test/java/dtu/scheduler/SetTimeFrameTest.java
@@ -17,38 +17,51 @@ public class SetTimeFrameTest {
 	private ErrorMessageHolder errorMessageHolder;
 	private Worker worker;
 	private String projectName = "timeframeTest", activityName = "timeframeActivity";
-	Project test_project;
+	Project testProject;
 
 	public SetTimeFrameTest(SchedulingApp schedulingApp, ErrorMessageHolder errorMessageHolder) throws Exception {
 		this.schedulingApp = schedulingApp;
 		this.errorMessageHolder = errorMessageHolder;
-		test_project = new Project(projectName);
+		testProject = new Project(projectName);
 	}
 	
 	@Given("There is a worker who is project leader for a given project")
 	public void there_is_a_worker_who_is_project_leader_for_a_given_project() throws WorkerDoesNotExistException, ProjectAlreadyExistsException, ProjectDoesNotExistException {
-		//worker = new Worker("ASDF");
 		schedulingApp.logIn("ASDF");
-		schedulingApp.addProject(test_project);
-		schedulingApp.assignProjectLeader(test_project.getProjectID(), "ASDF");
+		schedulingApp.addProject(testProject);
+		schedulingApp.assignProjectLeader(testProject.getProjectID(), "ASDF");
 	}
 
 	@Given("There is a project activity")
 	public void there_is_a_project_activity() throws Exception {
-		schedulingApp.createProjectActivity(activityName, test_project.getProjectID());
+		schedulingApp.createProjectActivity(activityName, testProject.getProjectID());
+	}
+	
+	@When("the project is given the time frame start year {int} start week {int} end year {int} end week {int}")
+	public void theProjectIsGivenTheTimeFrameStartYearStartWeekEndYearEndWeek(Integer startYear, Integer startWeek, Integer endYear, Integer endWeek) throws Exception {
+		testProject.setTimeFrame(startYear, startWeek, endYear, endWeek);
+	}
+
+	@Then("the project have a time frame with start year {int} start week {int} end year {int} end week {int}")
+	public void theProjectHaveATimeFrameWithStartYearStartWeekEndYearEndWeek(Integer startYear, Integer startWeek, Integer endYear, Integer endWeek) {
+		TimeFrame timeFrame = testProject.getTimeFrame();
+		assertTrue(timeFrame.getStartYear() == startYear);
+		assertTrue(timeFrame.getStartWeek() == startWeek);
+		assertTrue(timeFrame.getEndYear() == endYear);
+		assertTrue(timeFrame.getEndWeek() == endWeek);
 	}
 
 	@When("The project leader sets a time with current year, start week {int} and end week {int} for an activity")
 	public void theProjectLeaderSetsATimeWithCurrentYearStartWeekAndEndWeekForAnActivity(Integer startWeek, Integer endWeek) throws Exception {
 	    int currentYear = 2021;
-	    ProjectActivity curentActivity = test_project.searchActivity(activityName);
+	    ProjectActivity curentActivity = testProject.searchActivity(activityName);
 	    curentActivity.setTimeFrame(currentYear, startWeek, currentYear, endWeek);
 	}
 
 	@Then("The activity is given a time frame with current year, start week {int} and end week {int}")
 	public void theActivityIsGivenATimeFrameWithCurrentYearStartWeekAndEndWeek(Integer startWeek, Integer endWeek) {
 	    int currentYear = 2021;
-	    ProjectActivity curentActivity = test_project.searchActivity(activityName);
+	    ProjectActivity curentActivity = testProject.searchActivity(activityName);
 		int[] timeframe = curentActivity.getTimeframe();
 		assertTrue(timeframe[0] == currentYear);
 		assertTrue(timeframe[1] == startWeek);
@@ -58,7 +71,7 @@ public class SetTimeFrameTest {
 	
 	@When("The project leader sets an illegible time frame")
 	public void the_project_leader_sets_an_illegible_time_frame() {
-		ProjectActivity curentActivity = test_project.searchActivity(activityName);
+		ProjectActivity curentActivity = testProject.searchActivity(activityName);
 		try {
 			curentActivity.setTimeFrame(2021, -2, 2021, 55);
 		} catch (Exception e) {
--- use_cases/set_timeframe.feature
+++ use_cases/set_timeframe.feature
@@ -5,10 +5,14 @@ Feature: Set Timeframe
 	
 Background:
 	Given There is a worker who is project leader for a given project
+	
+Scenario: The project is given a time frame
+	# Mads Harder
+	When the project is given the time frame start year 2021 start week 1 end year 2021 end week 5
+	Then the project have a time frame with start year 2021 start week 1 end year 2021 end week 5
 
 Scenario: The project leader sets a time frame for a project activity
 	Given There is a project activity
-	
 	When The project leader sets a time with current year, start week 1 and end week 5 for an activity
 	Then The activity is given a time frame with current year, start week 1 and end week 5
 
