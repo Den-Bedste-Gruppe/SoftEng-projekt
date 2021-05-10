78cbc0849992a8afa5fd3e57fd6aabecc58b48c5
Mads Harder <mads987a@gmail.com>
2021-05-10 06:18:14 +0200

    Fixed and added some created by

--- src/test/java/dtu/scheduler/hasOverlapTest.java
+++ src/test/java/dtu/scheduler/hasOverlapTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+// Philip Hviid
 class hasOverlapTest {
 	private TimeFrame callerTimeFrame;
 	private TimeFrame parameterTimeFrame;
