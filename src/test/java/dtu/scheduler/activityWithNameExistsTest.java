704ccc08d4e599d3e549baa0820f3e62d58f9468
Mads Harder <mads987a@gmail.com>
2021-05-10 04:25:53 +0200

    Added activityWithNameExistsTest

---
+++ src/test/java/dtu/scheduler/activityWithNameExistsTest.java
@@ -0,0 +1,44 @@
+package dtu.scheduler;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class activityWithNameExistsTest {
+
+	static Project project;
+	
+	void addActivity(String name) throws Exception {
+		ProjectActivity activity = new ProjectActivity(name, project);
+		project.addActivity(activity);
+	}
+	
+	
+	@Test
+	public void testA() throws Exception {
+		project = new Project("project 1");
+		assertTrue(project.activityWithNameExists("act name") == false);
+	}
+
+	@Test
+	public void testB() throws Exception {
+		project = new Project("project 1");
+		addActivity("act name");
+		assertTrue(project.activityWithNameExists("act name") == true);
+	}
+
+	@Test
+	public void testC() throws Exception {
+		project = new Project("project 1");
+		addActivity("not act name");
+		assertTrue(project.activityWithNameExists("act name") == false);
+	}
+
+	@Test
+	public void testD() throws Exception {
+		project = new Project("project 1");
+		addActivity("not act name");
+		addActivity("act name");
+		assertTrue(project.activityWithNameExists("act name") == true);
+	}
+}
